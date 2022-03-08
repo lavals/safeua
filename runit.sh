4b81b7c25474c358349b1965d4fc1d152f186d6a
lavals <43458274+lavals@users.noreply.github.com>
2022-03-08 14:04:09 -0800

    added wget

--- runit.sh
+++ runit.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+
 FILENAME="list.txt"
+wget -O  $FILENAME https://raw.githubusercontent.com/lavals/safeua/main/list.txt
+
 LINES=$(cat $FILENAME)
 for LINE in $LINES
 do
-    docker run -it --rm alpine/bombardier -c 10 -d 5s -l "$LINE"
+    docker run -it --rm alpine/bombardier -c 10 -d 5s "$LINE"
 done
